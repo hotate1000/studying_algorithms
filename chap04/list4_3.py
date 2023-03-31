5bc509f5be62978fdbc10cae07cf976a775971ef
hotate1000 <ooo.336.sss@gmail.com>
2023-04-01 00:11:24 +0900

    スタックとキューのサンプル２

--- chap04/list4_3.py
+++ chap04/list4_3.py
@@ -40,3 +40,37 @@ def deque(self) -> Any:
     if self.front == self.capacity:
         self.front = 0;
     return x;
+
+def peek(self) -> Any:
+    if self.is_empty():
+        raise FixedQueue.Empty;
+    return self.que[self.front];
+
+def find(self, value: Any) -> Any:
+    for i in range(self.no):
+        idx = (i + self.front) % self.capacity;
+        if self.que[idx] == value:
+            return idx;
+    return -1;
+
+def count(self, value: Any) -> bool:
+    c = 0;
+    for i in range(self.no):
+        idx = (i + self.front) % self.capacity;
+        if self.que[idx] == value:
+            c += 1;
+    return c;
+
+def __contains__(self, value: Any) -> bool:
+    return self.count(value);
+
+def clear(self) -> None:
+    self.no = self.front = self.rear = 0;
+
+def dump(self) -> None:
+    if self.is_empty():
+        print('キューは空です。');
+    else:
+        for i in range(self.no):
+            print(self.que[(i + self.front) % self.capacity], end='');
+        print();
